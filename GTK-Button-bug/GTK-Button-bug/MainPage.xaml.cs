11ed8ac2e18390104d1f418e55b0a8cf65523f27
Martin Finkel <martin@videolabs.io>
2019-03-09 16:12:40 +0700

    add initial repro (not reproducing the issue)

---
+++ GTK-Button-bug/GTK-Button-bug/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GTK_Button_bug
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void nextButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GTK-Sample/Program.cs
@@ -0,0 +1,24 @@
+﻿using GTK_Button_bug;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace GTKSample
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            global::Gtk.Application.Init();
+            Forms.Init();
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.Show();
+
+            Gtk.Application.Run();
+        }
+    }
+}
